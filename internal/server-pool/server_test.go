9625020559221f6a0c8d4b2715ca99ad2c559161
Max Cotton <maxcotton22@gmail.com>
2025-07-03 23:29:09 +0100

    chore: Remove incorrect server.go test

--- internal/server-pool/server_test.go
+++ internal/server-pool/server_test.go
@@ -3,26 +3,9 @@
 package serverpool
 
 import (
-	"fmt"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
-// Handler to send HTTP requests
-func testRequestHandler (w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Test Helloo")
-}
-
-// Send test request to load balancer server and check response status
-func TestLoadBalancerRespondsOK(t *testing.T) {
-	r := httptest.NewRequest("GET", "http://localhost:8080", nil)
-	w := httptest.NewRecorder()
-	testRequestHandler(w, r)
-	response := w.Result()
-
-	if response.StatusCode != 200 {
-		t.Errorf("Status Code was %d, expected 200", response.StatusCode)
-	}
-}
+//TODO
+func TestNothing (t *testing.T) {}
 
