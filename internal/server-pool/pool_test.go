e9848abe004137db73d0aa81586050bf85f50173
Max Cotton <maxcotton22@gmail.com>
2025-07-03 14:20:11 +0100

    test: Setup server pool and use each node's forward method

--- internal/server-pool/pool_test.go
+++ internal/server-pool/pool_test.go
@@ -1,10 +1,38 @@
+// Tests for server.go
+
 package serverpool
 
 import (
+	"net/http/httptest"
 	"testing"
 )
 
-func TestNothing (t *testing.T) {
+// Setup test server pool and forward requests
+func TestServerPoolFunctionality (t *testing.T) {
 
+	// Initialise server pool
+	var urls [3]string = [3]string {
+		"http://localhost:8081",
+		"http://localhost:8082",
+		"http://localhost:8083",
+	}
+	var serverPool ServerPool = ServerPool{}
+	for _, url := range urls {
+		serverPool.Pool = append(serverPool.Pool, NewServerNode(url))
+	}
+
+	// Use each server node to forward HTTP request
+	for _, serverNode := range serverPool.Pool {	
+		r := httptest.NewRequest("GET", "http://localhost:8080", nil)
+		w := httptest.NewRecorder()
+		serverNode.ForwardRequest(w, r)
+		/*
+		response := w.Result()
+
+		if response.StatusCode != 200 {
+			t.Errorf("Status Code was %d, expected 200", response.StatusCode)
+		}
+		*/
+	}
 }
 
