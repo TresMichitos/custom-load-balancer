f13e5f9028ee28700143dba3db86d7e1d2f320b8
Max Cotton <maxcotton22@gmail.com>
2025-07-03 22:53:19 +0100

    chore: Add LbAlgorithm interface and setup server struct to use LbAlgorithm implementation to choose server node use

--- internal/server-pool/server.go
+++ internal/server-pool/server.go
@@ -1,22 +1,30 @@
-// Implementation of load balancer server struct
+// Implementation of load balancer server struct and definition of LbAlgorithm interface
 
 package serverpool
 
 import (
-	"fmt"
 	"net/http"
 )
 
-// Struct to represent load balancer server
-type Server struct {}
-
-// Handler function to send HTTP requests
-func requestHandler (w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Helloo")
+// Interface for load balancing algorithms
+type LbAlgorithm interface {
+	NextServerNode (*ServerPool) *ServerNode
 }
 
-func (server Server) StartLoadBalancer () {
-	http.HandleFunc("/", requestHandler)
+// Struct to represent load balancer server
+type Server struct {
+	ServerPool *ServerPool
+	LbAlgorithm LbAlgorithm
+}
+
+// Handler function to route HTTP request using balancing algorithm
+func (server *Server) requestHandler (w http.ResponseWriter, r *http.Request) {
+	var nextServerNode *ServerNode = server.LbAlgorithm.NextServerNode(server.ServerPool)
+	nextServerNode.ForwardRequest(w, r)
+}
+
+func (server *Server) StartLoadBalancer () {
+	http.HandleFunc("/", server.requestHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
