45bf978ca6a077424f59134f858381936bb34e58
CustordSolos <arthurstevens228@gmail.com>
2025-07-05 01:10:08 +0100

    feat: implement boilerplate http server.

--- demo/server.go
+++ demo/server.go
@@ -1,2 +1,26 @@
-package demo
+package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
+
+func main() {
+	// Honour $PORT if set; default to 8080.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	addr := ":" + port
+
+	http.HandleFunc("/", handler)
+
+	log.Printf("Server listening on %s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
