95a55f55c0c3683607a6a66c996fd3e692c0fb28
Max Cotton <maxcotton22@gmail.com>
2025-07-06 18:30:10 +0100

    chore: Setup basic integration test

---
+++ integration/integration_test.go
@@ -0,0 +1,21 @@
+package integration
+
+import (
+	"net/http"
+	"testing"
+)
+
+// Send requests to running load balancer and check responses
+func TestLoadBalancerFunctionality (t *testing.T) {
+	var lburl string = "http://localhost:8080"
+	resp, err := http.Get(lburl)
+	if err != nil {
+		t.Errorf("http.Get returned err %s", err.Error())
+	}
+
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Status code was %d, expected 200", resp.StatusCode)
+	}
+}
+
