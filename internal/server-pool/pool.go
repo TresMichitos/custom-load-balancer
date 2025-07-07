b942ee15131f782190213c5f0436d4862cd36a11
CustordSolos <arthurstevens228@gmail.com>
2025-07-07 19:21:51 +0100

    feat: add tracking of adctive connections per server node.

--- internal/server-pool/pool.go
+++ internal/server-pool/pool.go
@@ -13,33 +13,36 @@ import (
 
 // Struct to represent each node server we forward to
 type ServerNode struct {
-	URL string
-	ReverseProxy *httputil.ReverseProxy
-	mu sync.Mutex
+	URL               string
+	ReverseProxy      *httputil.ReverseProxy
+	ActiveConnections int
+	mu                sync.Mutex
 }
 
 // Factory function to initialise a new ServerNode object
-func NewServerNode (urlInput string) (*ServerNode, error) {
+func NewServerNode(urlInput string) (*ServerNode, error) {
 	url, err := url.Parse(urlInput)
 	if err != nil {
-		return nil, errors.New("Invalid URL")
+		return nil, errors.New("invalid URL")
 	}
 	return &ServerNode{URL: urlInput, ReverseProxy: httputil.NewSingleHostReverseProxy(url)}, nil
 }
 
 // Proxy function to forward HTTP request to server node
-func (serverNode *ServerNode) ForwardRequest (w http.ResponseWriter, r *http.Request) {
+func (serverNode *ServerNode) ForwardRequest(w http.ResponseWriter, r *http.Request) {
+	serverNode.ActiveConnections += 1
 	serverNode.ReverseProxy.ServeHTTP(w, r)
+	serverNode.ActiveConnections -= 1
 }
 
 // Struct to contain collection of server nodes
 type ServerPool struct {
 	Pool []*ServerNode
-	mu sync.Mutex
+	mu   sync.Mutex
 }
 
 // Factory function to initialise a new ServerPool object
-func NewServerPool (urls []string) *ServerPool {
+func NewServerPool(urls []string) *ServerPool {
 	var serverPool ServerPool
 	for _, url := range urls {
 		newServerNode, err := NewServerNode(url)
@@ -51,4 +54,3 @@ func NewServerPool (urls []string) *ServerPool {
 	}
 	return &serverPool
 }
-
