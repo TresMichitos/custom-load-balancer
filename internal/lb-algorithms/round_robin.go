8747f49877909f706343f1547520fd741bffff6d
Max Cotton <maxcotton22@gmail.com>
2025-07-19 16:34:23 +0100

    fix: Setup use of mutex lock for round robin index

--- internal/lb-algorithms/round_robin.go
+++ internal/lb-algorithms/round_robin.go
@@ -3,12 +3,14 @@
 package lbalgorithms
 
 import (
+	"sync"
 	serverpool "github.com/TresMichitos/custom-load-balancer/internal/server-pool"
 )
 
 // Struct to implement serverpool.LbAlgorithm interface
 type roundRobin struct {
 	index int
+	mu sync.Mutex
 }
 
 func NewRoundRobin() *roundRobin {
@@ -17,7 +19,9 @@ func NewRoundRobin() *roundRobin {
 
 // Select server node by iterating over server pool
 func (roundRobin *roundRobin) NextServerNode(serverPool *serverpool.ServerPool) *serverpool.ServerNode {
-	defer func() { roundRobin.index++ }()
+	defer func() { roundRobin.index++; roundRobin.mu.Unlock() }()
+
+	roundRobin.mu.Lock()
 
 	if roundRobin.index >= len(serverPool.Healthy) {
 		roundRobin.index = 0
