4a7011d3f9b8fe6ef8311491fc62d31572dd37fe
Electricmantaray <hlsj1066@gmail.com>
2025-07-21 00:25:40 +0100

    feat: Basic Ip hashing setup without algorithm

--- cmd/custom-load-balancer/main.go
+++ cmd/custom-load-balancer/main.go
@@ -51,6 +51,8 @@ func main() {
 		lbAlgorithm = lbAlgorithm.NewLeastUsedResources()
 	case "Random":
 		lbAlgorithm = lbalgorithms.NewRandom()
+	case "IpHashing":
+		lbAlgorithm = lbAlgorithm.NewIpHashing()
 	default:
 		lbAlgorithm = lbalgorithms.NewRoundRobin()
 	}
