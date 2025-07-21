eeba30e8300fc093ea4ef600e345af8a528bb324
Electricmantaray <hlsj1066@gmail.com>
2025-07-21 22:43:08 +0100

    Refactor: Updated next server node to ignore http Refactor

--- internal/lb-algorithms/round_robin.go
+++ internal/lb-algorithms/round_robin.go
@@ -3,14 +3,16 @@
 package lbalgorithms
 
 import (
+	"net/http"
 	"sync"
+
 	serverpool "github.com/TresMichitos/custom-load-balancer/internal/server-pool"
 )
 
 // Struct to implement serverpool.LbAlgorithm interface
 type roundRobin struct {
 	index int
-	mu sync.Mutex
+	mu    sync.Mutex
 }
 
 func NewRoundRobin() *roundRobin {
@@ -18,7 +20,7 @@ func NewRoundRobin() *roundRobin {
 }
 
 // Select server node by iterating over server pool
-func (roundRobin *roundRobin) NextServerNode(serverPool *serverpool.ServerPool) *serverpool.ServerNode {
+func (roundRobin *roundRobin) NextServerNode(serverPool *serverpool.ServerPool, _ *http.Request) *serverpool.ServerNode {
 	defer func() { roundRobin.index++; roundRobin.mu.Unlock() }()
 
 	roundRobin.mu.Lock()
