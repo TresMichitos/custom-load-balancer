19dd7fd9301e08538f752abac6e42c702edfab15
Electricmantaray <hlsj1066@gmail.com>
2025-07-21 19:57:31 +0100

    feat: Grabbing the IP Header

--- internal/lb-algorithms/ip_hashing.go
+++ internal/lb-algorithms/ip_hashing.go
@@ -3,6 +3,8 @@
 package lbalgorithms
 
 import (
+	"net/http"
+
 	serverpool "github.com/TresMichitos/custom-load-balancer/internal/server-pool"
 )
 
@@ -12,9 +14,15 @@ func NewIpHashing() *ipHashing {
 	return &ipHashing{}
 }
 
-func (ipHashing *ipHashing) NextServerNode(serverPool *serverpool.ServerPool) *serverpool.ServerNode {
+func (ipHashing *ipHashing) NextServerNode(serverPool *serverpool.ServerPool, req *http.Request) *serverpool.ServerNode {
 	// Server health check
 	if len(serverPool.Healthy) == 1 {
+		return serverPool.Healthy[0]
+	}
+
+	ipClient := req.Header.Get("X-Forwarded-For")
+
+	if ipClient == "" {
 		return serverPool.Healthy[0]
 	}
 
