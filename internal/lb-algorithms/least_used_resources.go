84638e8deb2eb01569265aa239edb39a54cdaeb1
Electricmantaray <hlsj1066@gmail.com>
2025-08-02 04:25:47 +0100

    Refactor: updated the algorithm and weightings, removed debugging statements

--- internal/lb-algorithms/least_used_resources.go
+++ internal/lb-algorithms/least_used_resources.go
@@ -3,7 +3,7 @@
 package lbalgorithms
 
 import (
-	"log"
+	"math"
 	"net/http"
 
 	dockerstats "github.com/TresMichitos/custom-load-balancer/internal/dockerstats"
@@ -16,6 +16,8 @@ func NewLeastUsedResources() *leastUsedResources {
 	return &leastUsedResources{}
 }
 
+// Selects the healthiest node based on weighted cpu and memory usage
+// Using 60/40 weighting rule
 func (leastUsedResources *leastUsedResources) NextServerNode(serverPool *serverpool.ServerPool, _ *http.Request) *serverpool.ServerNode {
 	if len(serverPool.Healthy) == 1 {
 		return serverPool.Healthy[0]
@@ -24,41 +26,29 @@ func (leastUsedResources *leastUsedResources) NextServerNode(serverPool *serverp
 	stats, err := dockerstats.GetDockerStats()
 
 	if err != nil {
-		log.Printf("There was an error, %v", err)
 		return serverPool.Healthy[0]
 	}
 
 	var bestnode *serverpool.ServerNode
-	var lowestScore float64 = 1000
+	lowestScore := math.MaxFloat64
 
 	for _, node := range serverPool.Healthy {
-		log.Printf("container nhame: %s ", node.ContainerName)
-
-		for k := range stats {
-			log.Printf("container stats: %s ", k)
-		}
 
 		stat, ok := stats[node.ContainerName]
 		if !ok {
 			continue
 		}
-		log.Printf("Container %s CPU: %.2f%%, Mem: %.2f%%", stat.Name, stat.CPUPerc, stat.MemPerc)
-		cpu := stat.CPUPerc
-		mem := stat.MemPerc
 
 		// Applying 60/40 weighting
-		score := (cpu * 0.6) + (mem * 0.4)
-		log.Printf("score: %f", score)
+		score := (stat.CPUPerc * 0.6) + (stat.MemPerc * 0.4)
 
 		if score < lowestScore {
 			bestnode = node
 			lowestScore = score
-			log.Printf("lowestscore: %f", lowestScore)
 		}
 	}
 
 	if bestnode == nil {
-		log.Printf("No suitable node found by resource stats; using fallback node")
 		return serverPool.Healthy[0]
 	}
 
